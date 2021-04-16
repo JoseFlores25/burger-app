54540b78ccba570140fd2dfc7764525deb0d7abb
Jose <flores.jose2185@gmail.com>
2021-04-15 20:52:33 -0400

    prop-type

---
+++ src/components/Burger/Burgeringredient.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import "./BurgerIngredient.css";
+
+const burgerIngredient = (props) => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case "bread-bottom":
+      ingredient = <div className={BreadBottom}></div>;
+      break;
+    case "bread-top":
+      ingredient = (
+        <div className={BreadTop}>
+          <div className={Seeds1}></div>
+          <div className={Seeds2}></div>
+        </div>
+      );
+      break;
+    case "meat":
+      ingredient = <div className={Meat}></div>;
+      break;
+    case "meat":
+      ingredient = <div className={Meat}></div>;
+      break;
+    case "meat":
+      ingredient = <div className={Meat}></div>;
+      break;
+    case "meat":
+      ingredient = <div className={Meat}></div>;
+      break;
+  }
+};
+
+export default burgerIngredient;
